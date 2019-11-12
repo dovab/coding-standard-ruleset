ae73f7de2fe10656acdbb17b4dcc1a4ac7dbc86d
Wouter Carabain <dovab@users.noreply.github.com>
2019-11-12 13:51:24 +0100

    Updated the install script to be more robust

--- install-rulesets.sh
+++ install-rulesets.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Installing PHPCS Rulesets..."
-vendor/bin/phpcs --config-set installed_paths vendor/escapestudios/symfony2-coding-standard,vendor/dovab/coding-standard-rules
+DIR="$(cd "$(dirname "$1")"; pwd -P)"
+$DIR/vendor/bin/phpcs --config-set installed_paths $DIR/vendor/escapestudios/symfony2-coding-standard,$DIR/vendor/dovab/coding-standard-rules
